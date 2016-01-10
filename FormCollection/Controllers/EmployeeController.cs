e80b3067502b6a8477e56fe1a85d18cb3f0181a0
techstrikers <ajaysaptaputre@techstrikers.com>
2016-01-10 14:13:49 +0530

    CheckIn

---
+++ FormCollection/Controllers/EmployeeController.cs
@@ -0,0 +1,72 @@
+﻿using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCFormCollection.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //
+        // GET: /Employee/
+        public ActionResult Index()
+        {
+            DataAccessLib db = new DataAccessLib();
+            return View(db.GetEmployees);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Create")]
+        public ActionResult formCollection(FormCollection formCollection)
+        {
+            Employee emp = new Employee();
+            DataAccessLib db = new DataAccessLib();
+            emp.Name = formCollection["Name"];
+            emp.Designation = formCollection["Designation"];
+            emp.Gender = formCollection["Gender"];
+            emp.Salary = Convert.ToDouble(formCollection["Salary"]);
+            emp.City = formCollection["City"];
+            emp.State = formCollection["State"];
+            emp.Zip = formCollection["Zip"];
+
+            db.AddEmployees(emp);
+
+            return RedirectToAction("Index");
+        }
+
+        //[HttpPost]
+        //[ActionName("Create2")]
+        //public ActionResult employeeObject(Employee emp)
+        //{
+        //    DataAccessLib db = new DataAccessLib();
+        //    db.AddEmployees(emp);
+        //    return RedirectToAction("Index");
+        //}
+
+        //[HttpPost]
+        //[ActionName("Create3")]
+        //public ActionResult usingParameter(string name, string designation, string gender, double salary, string city, string state, string zip)
+        //{
+        //    Employee emp = new Employee();
+        //    DataAccessLib db = new DataAccessLib();
+        //    emp.Name = name;
+        //    emp.Designation = designation;
+        //    emp.Gender = gender;
+        //    emp.Salary = salary;
+        //    emp.City = city;
+        //    emp.State = state;
+        //    emp.Zip = zip;
+        //    db.AddEmployees(emp);
+
+        //    return RedirectToAction("Index");
+        //}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Access-values-in-formcollection-from-controller-action-method
-In this article I am going to discuss how to map form posted request data to controller's specific action method. How FormCollection object help us to access the posted data in controller action method. &lt;a href="http://www.techstrikers.com/Articles/access-values-in-formcollection-from-controller-action-method.php">http://www.techstrikers.com/Articles/access-values-in-formcollection-from-controller-action-method.php&lt;a>
+In this article I am going to discuss how to map form posted request data to controller's specific action method. How FormCollection object help us to access the posted data in controller action method.  
+To read this article visit: http://www.techstrikers.com/Articles/access-values-in-formcollection-from-controller-action-method.php
